72e9336077312455874a5c0ab6cb9fbc81f589e4
Conor Barnes <conorbarnes93@gmail.com>
2020-06-28 15:22:08 -0700

    Add skeleton and test function (print timeline)

--- unliker.py
+++ unliker.py
@@ -1,2 +1,16 @@
 import tweepy
 
+privatefile = open('nothankyou.txt', 'r')
+consumer_key = privatefile.readLine()
+consumer_secret = privatefile.readLine()
+access_token = privatefile.readLine()
+access_token_secret = privatefile.readLine()
+privatefile.close()
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+timeline = api.user_timeline("ArielBissett")
+
+print(timeline)
