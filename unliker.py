26becbc6c49cb2396190d995a31e3e6d43a3e1d4
Conor Barnes <conorbarnes93@gmail.com>
2020-06-29 09:28:46 -0700

    Clean up extraneous code

--- unliker.py
+++ unliker.py
@@ -6,9 +6,6 @@ import tweepy
 privatefile = open('nothankyou.txt', 'r')
 consumer_key = privatefile.readline().strip()
 consumer_secret = privatefile.readline().strip()
-test = privatefile.readline().strip()
-# access_token = privatefile.readline()
-#access_token_secret = privatefile.readline()
 privatefile.close()
 
 print(consumer_key, consumer_secret, test)
@@ -25,8 +22,6 @@ print('ACCESS_SECRET = "%s"' % auth.access_token_secret)
 auth.set_access_token(auth.access_token, auth.access_token_secret)
 
 api = tweepy.API(auth)
-timeline = api.user_timeline("ArielBissett")
 username = api.me()
 username = username["name"]
 print(username)
-print(timeline)
