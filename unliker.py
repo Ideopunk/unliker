612353cd41b30dc51631d1fd86b6b177ad0eb075
Conor Barnes <conorbarnes93@gmail.com>
2020-06-29 09:04:31 -0700

    update based on https://gist.github.com/hezhao/4772180

--- unliker.py
+++ unliker.py
@@ -1,16 +1,26 @@
+# import os
 import tweepy
 
 privatefile = open('nothankyou.txt', 'r')
 consumer_key = privatefile.readLine()
 consumer_secret = privatefile.readLine()
-access_token = privatefile.readLine()
-access_token_secret = privatefile.readLine()
+# access_token = privatefile.readLine()
+#access_token_secret = privatefile.readLine()
 privatefile.close()
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret, callback='oob')
+auth_url = auth.get_authorization_url()
+print('Authorization URL: ' + auth_url)
+
+verifier = input('PIN: ').strip()
+auth.get_access_token(verifier)
+print('ACCESS_KEY = "%s"' % auth.access_token)
+print('ACCESS_SECRET = "%s"' % auth.access_token_secret)
+
+auth.set_access_token(auth.access_token, auth.access_token_secret)
 
 api = tweepy.API(auth)
 timeline = api.user_timeline("ArielBissett")
-
+username = api.me()
+print(username)
 print(timeline)
